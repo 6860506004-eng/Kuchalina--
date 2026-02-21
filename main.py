691a94e84ee8ddb8cc5255fdf0afe0597b620943
6860506004-eng <6860506004@pnu.ac.th>
2026-02-21 18:42:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 from flask import Flask, render_template
-import pymysql
 
 app = Flask(__name__)
 
-
-db_config = {
-    'host': 's6860506004db-kuchalina-hwzz2w',
-    'user': 'Nina6860506004',
-    'password': '1859900347014',
-    'database': 'Nina',
-    'cursorclass': pymysql.cursors.DictCursor
-}
-
 @app.route('/')
 def index():
-    try:
-        connection = pymysql.connect(**db_config)
-        with connection.cursor() as cursor:
-          
-            cursor.execute("SELECT * FROM Flower")
-            flower_list = cursor.fetchall()
-        connection.close()
-        return render_template('index.html', flowers=flower_list)
-    except Exception as e:
-        return f"<h1>Error:</h1><p>{str(e)}</p>"
+    
+    flower_test = [
+        {'variety_name': 'Test Rose', 'color': 'Red', 'price': 100, 'origin': 'Thailand', 'blooming_season': 'Summer'}
+    ]
+    return render_template('index.html', flowers=flower_test)
 
 if __name__ == '__main__':
    
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
