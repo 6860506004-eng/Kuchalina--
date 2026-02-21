c848cec4bbe8956bc4f9cde2fdeb22299f75abe3
6860506004-eng <6860506004@pnu.ac.th>
2026-02-21 18:22:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 
 db_config = {
-    'host': 's6860506004db-kuchalina-hwzz2w', 
-    'user': 'Nina6860506004',                 
-    'password': '1859900347014',              
-    'database': 'Nina',                       
+    'host': 's6860506004db-kuchalina-hwzz2w',
+    'user': 'Nina6860506004',
+    'password': '1859900347014',
+    'database': 'Nina',
     'cursorclass': pymysql.cursors.DictCursor
 }
 
@@ -17,13 +17,13 @@ def index():
     try:
         connection = pymysql.connect(**db_config)
         with connection.cursor() as cursor:
-            
+          
             cursor.execute("SELECT * FROM Flower")
             flower_list = cursor.fetchall()
         connection.close()
         return render_template('index.html', flowers=flower_list)
     except Exception as e:
-        return f"<h1>เกิดข้อผิดพลาดในการเชื่อมต่อฐานข้อมูล:</h1><p>{str(e)}</p>"
+        return f"<h1>Error:</h1><p>{str(e)}</p>"
 
 if __name__ == '__main__':
    
