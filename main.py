3f4869152c933ac32ecf72d91d3b4d3cb8eb3c73
6860506004-eng <6860506004@pnu.ac.th>
2026-02-21 18:46:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    
-    flower_test = [
-        {'variety_name': 'Test Rose', 'color': 'Red', 'price': 100, 'origin': 'Thailand', 'blooming_season': 'Summer'}
-    ]
-    return render_template('index.html', flowers=flower_test)
+def home():
+    return "<h1> Nina's Server is Online! </h1> <p> ถ้าเห็นข้อความนี้ แสดงว่าพอร์ต 8000 ทำงานแล้วครับ </p>"
 
 if __name__ == '__main__':
-   
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    
+    app.run(host='0.0.0.0', port=8000)
