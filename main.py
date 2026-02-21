afa856666f3251093d06241ab6ce7d028077952e
6860506004-eng <6860506004@pnu.ac.th>
2026-02-21 19:23:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1> Nina's Server is Online! </h1> <p> ถ้าเห็นข้อความนี้ แสดงว่าพอร์ต 8000 ทำงานแล้วครับ </p>"
+  
+    return "<h1> Nina's Server is finally Online! </h1>"
 
 if __name__ == '__main__':
-    
     app.run(host='0.0.0.0', port=3000)
